768d50ed240042a27b5032a7c89b4ca610ad16bf
osamoya <osamumoyamoyasii@yahoo.co.jp>
2021-06-22 20:59:17 +0900

    [Add]茶柱スクリプトの大枠を作成
    MoveForceは、場所を引数にとり呼び出される。
    中の処理はまだ未定

--- Assets/Scripts/Tyabasira_Script.cs
+++ Assets/Scripts/Tyabasira_Script.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Tyabasira_Script : MonoBehaviour
 {
+    private float angleX { get; }
+    private float angleY { get; }
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void MoveForce(int n)//場所１～４
+    {
+        Debug.Log("MoveForce呼び出され："+n);
     }
 }
