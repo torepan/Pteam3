02969b4b4eb75d2594bcdd7a61eafbc9ed0dbf58
S1923032\imdon <imdonguri@gmail.com>
2021-06-23 00:31:28 +0900

    いろいろ作った

---
+++ Assets/Scripts/PActionTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PActionTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 左ボタンクリック
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            Debug.Log("LeftClick:" + mousePosition);
+        }
+        // 右ボタンクリック
+        if (Input.GetMouseButtonUp(1))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            Debug.Log("RightClick:" + mousePosition);
+        }
+        // 中ボタンクリック
+        if (Input.GetMouseButtonUp(2))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            Debug.Log("MiddleClick:" + mousePosition);
+        }
+    }
+}
