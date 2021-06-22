23fcbe7d4bb08470e05bf1d00dca58032d010c82
osamoya <osamumoyamoyasii@yahoo.co.jp>
2021-06-22 21:08:46 +0900

    [Add]PlayerActioのガワを作成
    必要な関数などは一応そろっている
    妖精のスクリプトができ次第差し替えが必要

---
+++ Assets/Scripts/PlayerAction_Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction_Script : MonoBehaviour
+{
+    [SerializeField] Tyabasira_Script Tyabasira_;
+    //[SerializeField] YOUSEI sousei;
+    public void OnClickDo(int n)
+    {
+        if (!true/*yousei.CanPush*/) return;
+
+        if (n != 1) moveChar(n);
+        if (n == 1) moveTyaba(n);
+    }
+
+    void moveChar(int n)
+    {
+
+    }
+
+    void moveTyaba(int n)
+    {
+        Tyabasira_.MoveForce(n);//PUN2の関係で最初から切り出す
+    }
+}
