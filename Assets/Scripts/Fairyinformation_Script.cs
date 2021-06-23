2f28137ff13c74c66f05f3db5e41b8898f0f5010
1923129-PC\1923129 <maeriku165021@gmail.com>
2021-06-23 20:25:09 +0900

    Fairyinformation_Scriptに変数を追加。

--- Assets/Scripts/Fairyinformation_Script.cs
+++ Assets/Scripts/Fairyinformation_Script.cs
@@ -7,12 +7,21 @@ public class Fairyinformation_Script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int place; //場所
+        bool canPush; //移動中や準備中なら押せない
+        float montionTime; //時間にかかる時間を計る
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        void MoveReceive(){
+
+        }
+
+        void canPushTime(){
+            
+        }
+
     }
 }
