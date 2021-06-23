e7e96999fcc2e39d211104ed42897ab63ae2b428
S1923032\imdon <imdonguri@gmail.com>
2021-06-23 18:47:14 +0900

    ボタン作った

--- Assets/Scripts/PActionTest.cs
+++ Assets/Scripts/PActionTest.cs
@@ -13,23 +13,11 @@ public class PActionTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // 左ボタンクリック
-        if (Input.GetMouseButtonUp(0))
-        {
-            Vector3 mousePosition = Input.mousePosition;
-            Debug.Log("LeftClick:" + mousePosition);
-        }
-        // 右ボタンクリック
-        if (Input.GetMouseButtonUp(1))
-        {
-            Vector3 mousePosition = Input.mousePosition;
-            Debug.Log("RightClick:" + mousePosition);
-        }
-        // 中ボタンクリック
-        if (Input.GetMouseButtonUp(2))
-        {
-            Vector3 mousePosition = Input.mousePosition;
-            Debug.Log("MiddleClick:" + mousePosition);
-        }
+        
+    }
+
+    public void TestAction(int n)
+    {
+        Debug.Log(n);
     }
 }
