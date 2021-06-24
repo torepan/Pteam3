e2743b0ba22cb82bcadbeab3a75a9a60e149dd0d
1923129-PC\1923129 <maeriku165021@gmail.com>
2021-06-24 11:32:14 +0900

    Fairyinformation_Scriptが一応完成しました。

--- Assets/Scripts/Fairyinformation_Script.cs
+++ Assets/Scripts/Fairyinformation_Script.cs
@@ -4,24 +4,32 @@ using UnityEngine;
 
 public class Fairyinformation_Script : MonoBehaviour
 {
+    public int place; //場所
+    public bool canPush; //移動中や準備中なら押せない
+    float montionTime; //移動にかかる時間を計る
+    float interval=5;//移動時間
     // Start is called before the first frame update
     void Start()
     {
-        int place; //場所
-        bool canPush; //移動中や準備中なら押せない
-        float montionTime; //時間にかかる時間を計る
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        void MoveReceive(){
-
+        
+    }
+    public void MoveReceive(int n){
+            place=n;
+            canPush=false;
+            canPushTime();
         }
 
         void canPushTime(){
-            
+            while(montionTime<interval){
+                montionTime+= Time.deltaTime;
+            }
+            montionTime=0;
+            canPush=true;
         }
-
-    }
 }
