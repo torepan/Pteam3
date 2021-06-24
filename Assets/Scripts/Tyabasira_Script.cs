ca8eb41bc6a24e6fb203270b588f5e013a04db04
osamoya <osamumoyamoyasii@yahoo.co.jp>
2021-06-24 11:41:29 +0900

    [fix]茶柱の姿勢に関する変数の名前と宣言を修正
    Y→Z
    プロパティを使いたかったので、private→publicにし、setに制限をかけてみた。
    修正はある

--- Assets/Scripts/Tyabasira_Script.cs
+++ Assets/Scripts/Tyabasira_Script.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Tyabasira_Script : MonoBehaviour
 {
-    private float angleX { get; }
-    private float angleY { get; }
+    public float angleX { private set; get; }
+    public float angleZ { private set; get; }
     Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        angleX = transform.eulerAngles.x;
+        angleZ = transform.eulerAngles.z;
     }
 
     // Update is called once per frame
@@ -22,5 +24,8 @@ public class Tyabasira_Script : MonoBehaviour
     public void MoveForce(int n)//場所１～４
     {
         Debug.Log("MoveForce呼び出され："+n);
+
+        
+
     }
 }
