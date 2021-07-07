5794ce418d41fd37690f8ce6d42846a1b523904b
osamoya <osamumoyamoyasii@yahoo.co.jp>
2021-07-08 02:30:17 +0900

    [update]クリア判定のスクリプトを作成
    ランクの閾値をインスペクターから設定
    終了時間も設定
    CalcRankを呼び出すと整数でランクが返ってくる

--- Assets/Scripts/ClearCheck_Script.cs
+++ Assets/Scripts/ClearCheck_Script.cs
@@ -8,28 +8,55 @@ using UnityEngine;
 public class ClearCheck_Script: MonoBehaviour
 {
     [SerializeField] Tyabasira_Script Tyabasira_;
-    [SerializeField] int[] rank=new int[4];//SABC
+    [SerializeField] int timeLimit=10;
+    [SerializeField] int[] rankThreshold = new int[4];//SABC
+    bool isFin = false;//終了しているかどうか
     float timeCount = 0f;//開始からの時間
 
     void TimeReset()
     {
-
+        isFin = false;
+        timeCount = 0f;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        TimeReset();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!isFin)
+        {
+            if (timeCount <= timeLimit) timeCount += Time.deltaTime;
+            else calcRank();
+        }
         
     }
 
-    void calcRank()
+    public int CalcRank()
     {
-        
+        float angleXZ = CalcAngle() ;
+        int rank = 0;
+        for (int i=0;i<rankThreshold.Length;i++)
+        {
+            if (angleXZ<=rankThreshold[i])
+            {
+                rank = i;
+                break;
+            }
+        }
+        return rank;
+    }
+
+    float CalcAngle()
+    {
+        float x = Tyabasira_.angleX;
+        float z = Tyabasira_.angleZ;
+
+        return (x > z) ? x : z;//大きいほうを返すのでよいのでは？
+        //return -1;
     }
 }
