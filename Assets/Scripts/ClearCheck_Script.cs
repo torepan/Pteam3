e13c219c77d2f85eaba2bfcdf08926bb73061fc2
osamoya <osamumoyamoyasii@yahoo.co.jp>
2021-07-08 02:16:23 +0900

    [Add]クリア判定用のスクリプトのガワを作成

---
+++ Assets/Scripts/ClearCheck_Script.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 茶柱角度からクリアの判定
+/// タイマー機能
+/// </summary>
+public class ClearCheck_Script: MonoBehaviour
+{
+    [SerializeField] Tyabasira_Script Tyabasira_;
+    [SerializeField] int[] rank=new int[4];//SABC
+    float timeCount = 0f;//開始からの時間
+
+    void TimeReset()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void calcRank()
+    {
+        
+    }
+}
