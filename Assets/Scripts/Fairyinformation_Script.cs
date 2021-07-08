0009b658181b12913ebeccdf78e9669e80e9bbb2
osamoya <osamumoyamoyasii@yahoo.co.jp>
2021-07-08 11:25:50 +0900

    [Fix]インデントの調整とデバッグ用に数値を変更

--- Assets/Scripts/Fairyinformation_Script.cs
+++ Assets/Scripts/Fairyinformation_Script.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Fairyinformation_Script : MonoBehaviour
 {
     public int place; //場所
-    public bool canPush=true; //移動中や準備中なら押せない
+    public bool canPush = true; //移動中や準備中なら押せない
     float montionTime; //移動にかかる時間を計る
     [SerializeField]
-    float interval=5;//移動時間
-    int testPlace=1;//テスト用変数
+    float interval = 0.1f;//移動時間
+    int testPlace = 1;//テスト用変数
     // Start is called before the first frame update
     void Start()
     {
@@ -20,38 +20,46 @@ public class Fairyinformation_Script : MonoBehaviour
     void Update()
     {
         //次の移動までの時間を計測
-        montionTime+= Time.deltaTime;
+        
         //canPushがtrueなら
-        if(canPush==true){
-        //テスト用のコード
-        //スペースキーを押すとtestPlaceの値が増えてplaceの値が変わる
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            testPlace++;
-            MoveReceive(testPlace);
-            Debug.Log(place);
+        if (canPush == true)
+        {
+            //テスト用のコード
+            //スペースキーを押すとtestPlaceの値が増えてplaceの値が変わる
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                testPlace++;
+                MoveReceive(testPlace);
+                Debug.Log(place);
+            }
         }
-        }else{
-        //ここでintervalよりmontionTimeの数値が大きいと
-        if(montionTime>=interval){
-        Debug.Log("準備");
-        canPushTime();
-        }
+        else
+        {
+            montionTime += Time.deltaTime;
+            //ここでintervalよりmontionTimeの数値が大きいと
+            if (montionTime >= interval)
+            {
+                Debug.Log("準備");
+                canPushTime();
+            }
         }
     }
 
     //受け取った引数をplaceに入れて、canPushをfalseに
-    public void MoveReceive(int n){
-            place=n;
-            canPush=false;
-            montionTime=0;
-            Debug.Log(canPush);
-        }
+    public void MoveReceive(int n)
+    {
+        place = n;
+        canPush = false;
+        montionTime = 0;
+        Debug.Log(canPush);
+    }
 
-        //canPushTimeはcanPushをtrueにするだけ
-        void canPushTime(){
-            Debug.Log("動ける");
-            montionTime=0;
-            canPush=true;
-            Debug.Log(canPush);
-        }
+    //canPushTimeはcanPushをtrueにするだけ
+    void canPushTime()
+    {
+        Debug.Log("動ける");
+        montionTime = 0;
+        canPush = true;
+        Debug.Log(canPush);
+    }
 }
