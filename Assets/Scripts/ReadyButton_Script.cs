35ffdf5d61dd86e00817f28657db8f06d314181a
1923129-PC\1923129 <maeriku165021@gmail.com>
2021-08-03 00:03:00 +0900

    ボタンを押すと茶柱が出現するスクリプトを作成。ここから3秒間カウントしてから出現するようにする。

---
+++ Assets/Scripts/ReadyButton_Script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadyButton_Script : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Tyabasira;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Ready(){
+        Debug.Log("準備完了");
+        Tyabasira.SetActive (true);
+    }
+}
