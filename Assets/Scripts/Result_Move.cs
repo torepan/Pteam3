d0f8b14fd15898f93e8c250257e741d34ceb6f08
YamakawaSaki <ymsaki377@gmail.com>
2021-08-03 14:35:20 +0900

    ？

---
+++ Assets/Scripts/Result_Move.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Result_Move : MonoBehaviour
+{
+
+    //text
+    public GameObject FinishText;
+    public GameObject ResultDammy;
+    //object
+    public GameObject Tyabashira;
+    public GameObject cam;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //ゲーム終了時に呼び出す関数
+    public void Result()
+    {
+        
+        //茶柱の角度を求める
+
+        FinishText.SetActive(true);
+        
+        //カメラ移動
+
+
+        FairyRun();
+
+        //呼び出す演出の選択
+
+        Directing();
+
+    }
+
+    //妖精が退散する関数
+    private void FairyRun()
+    {
+
+    }
+
+    //演出を呼び出す関数
+    private void Directing()
+    {
+        ResultDammy.SetActive(true);
+    }
+
+ //--------デバッグ用-----------
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("NakazawaScene");
+    }
+
+}
---
+++ Assets/Scripts/TyabasiraFailure_Script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// /// 子供のy座標を比較して、自分より下だったら終了を飛ばす
+/// </summary>
+public class TyabasiraFailure_Script : MonoBehaviour
+{
+    ClearCheck_Script ClearCheck_;
+    [SerializeField] GameObject checkPoint;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (checkPoint.transform.position.y < transform.position.y)
+        {
+            Debug.Log("横になった！");
+            ///
+        }
+    }
+}
