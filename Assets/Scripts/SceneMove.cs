9ceec90b1df2565ee4652a7f4a89b9056689bdbc
S1923032\imdon <imdonguri@gmail.com>
2021-08-13 02:38:02 +0900

    開始ボタンとシーン遷移用スクリプトのガワ作っただけ

---
+++ Assets/Scripts/SceneMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //待機画面へ遷移するボタン用
+    public void MoveMain()
+    {
+
+    }
+    
+    //タイトルへ遷移するボタン用
+    public void MoveTitle()
+    {
+       
+    }
+}
